fd30e6456ca299d2db62fbcca4a26c5438e2904c
Binh Vu <binh@toan2.com>
2021-11-16 14:33:26 -0800

    sync with kgdata

--- sm_widgets_integration/wikidata.py
+++ sm_widgets_integration/wikidata.py
@@ -56,19 +56,23 @@ class GramsIntFn:
         for pid, stmts in qnode.props.items():
             new_stmts = []
             for stmt in stmts:
-                new_stmt = Statement(value=GramsIntFn.wd_value_deser(stmt.value),
-                                     qualifiers={
-                                         f'http://www.wikidata.org/prop/{qid}': [GramsIntFn.wd_value_deser(x) for x in lst]
-                                         for qid, lst in stmt.qualifiers.items()
-                                     })
+                new_stmt = Statement(
+                    value=GramsIntFn.wd_value_deser(stmt.value),
+                    qualifiers={
+                        f"http://www.wikidata.org/prop/{qid}": [
+                            GramsIntFn.wd_value_deser(x) for x in lst
+                        ]
+                        for qid, lst in stmt.qualifiers.items()
+                    },
+                )
                 new_stmts.append(new_stmt)
-            props[f'http://www.wikidata.org/prop/{pid}'] = new_stmts
+            props[f"http://www.wikidata.org/prop/{pid}"] = new_stmts
         return GramsIntFn.WrapperQNode(
             id=qnode.id,
             uri=f"http://www.wikidata.org/entity/{qnode.id}",
             label=qnode.label,
             description=qnode.description,
-            properties=props
+            properties=props,
         )
 
     @staticmethod
@@ -77,9 +81,15 @@ class GramsIntFn:
         parents_closure = {
             f"http://www.wikidata.org/entity/{p}" for p in item.parents_closure
         }
-        return GramsIntFn.WrapperWDClass(id=item.id, uri=item.get_uri(), aliases=item.aliases,
-                                         label=item.label, description=item.description,
-                                         parents=parents, parents_closure=parents_closure)
+        return GramsIntFn.WrapperWDClass(
+            id=item.id,
+            uri=item.get_uri(),
+            aliases=item.aliases,
+            label=item.label,
+            description=item.description,
+            parents=parents,
+            parents_closure=parents_closure,
+        )
 
     @staticmethod
     def ont_prop_deser(item: WDProperty):
@@ -87,14 +97,20 @@ class GramsIntFn:
         parents_closure = {
             f"http://www.wikidata.org/entity/{p}" for p in item.parents_closure
         }
-        return GramsIntFn.WrapperWDProperty(id=item.id, uri=item.get_uri(), aliases=item.aliases,
-                                            label=item.label, description=item.description,
-                                            parents=parents, parents_closure=parents_closure)
+        return GramsIntFn.WrapperWDProperty(
+            id=item.id,
+            uri=item.get_uri(),
+            aliases=item.aliases,
+            label=item.label,
+            description=item.description,
+            parents=parents,
+            parents_closure=parents_closure,
+        )
 
     @staticmethod
     def wd_value_deser(val: DataValue):
-        if val.is_qnode():
-            ent_id = val.as_qnode_id()
+        if val.is_entity_id():
+            ent_id = val.as_entity_id()
             if ent_id.startswith("Q"):
                 uri = f"http://www.wikidata.org/entity/{ent_id}"
             elif ent_id.startswith("P"):
@@ -103,11 +119,11 @@ class GramsIntFn:
                 uri = ent_id
             return Value(type=ValueType.URI, value=uri)
         if val.is_quantity():
-            return Value(type=ValueType.Float, value=val.value['amount'])
+            return Value(type=ValueType.Float, value=val.value["amount"])
         return Value(type=ValueType.String, value=val.to_string_repr())
 
 
-def get_qnode_db(db_or_dbfile, read_only=False, proxy: bool=False):
+def get_qnode_db(db_or_dbfile, read_only=False, proxy: bool = False):
     if isinstance(db_or_dbfile, (str, Path)):
         db = kg_db.get_qnode_db(db_or_dbfile, read_only=read_only, proxy=proxy)
     else:
@@ -116,7 +132,7 @@ def get_qnode_db(db_or_dbfile, read_only=False, proxy: bool=False):
     return StoreWrapper(db, GramsIntFn.key_deser, GramsIntFn.qnode_deser)
 
 
-def get_ontclass_db(db_or_dbfile: str, read_only=False, proxy: bool=False):
+def get_ontclass_db(db_or_dbfile: str, read_only=False, proxy: bool = False):
     if isinstance(db_or_dbfile, (str, Path)):
         db = kg_db.get_wdclass_db(db_or_dbfile, read_only=read_only, proxy=proxy)
     else:
@@ -125,7 +141,7 @@ def get_ontclass_db(db_or_dbfile: str, read_only=False, proxy: bool=False):
     return StoreWrapper(db, GramsIntFn.key_deser, GramsIntFn.ont_class_deser)
 
 
-def get_ontprop_db(db_or_dbfile: str, read_only=False, proxy: bool=False):
+def get_ontprop_db(db_or_dbfile: str, read_only=False, proxy: bool = False):
     if isinstance(db_or_dbfile, (str, Path)):
         db = kg_db.get_wdprop_db(db_or_dbfile, read_only=read_only, proxy=proxy)
     else:
@@ -143,18 +159,20 @@ def index_ontology(eshost: str):
             "label": str(wdprop.label),
             "readable_label": f"{wdprop.label} ({wdprop.id})",
             "aliases": [str(x) for x in wdprop.aliases],
-            "description": str(wdprop.description)
+            "description": str(wdprop.description),
         }
         for wdprop in wdprops.values()
     ]
-    docs.append({
-        "id": str(RDFS.label),
-        "uri": str(RDFS.label),
-        "label": "rdfs:label",
-        "readable_label": "rdfs:label",
-        "aliases": ["label"],
-        "description": ["label of a resource"]
-    })
+    docs.append(
+        {
+            "id": str(RDFS.label),
+            "uri": str(RDFS.label),
+            "label": "rdfs:label",
+            "readable_label": "rdfs:label",
+            "aliases": ["label"],
+            "description": ["label of a resource"],
+        }
+    )
     OntologyPropertySearch(eshost).load2index(docs)
 
     wdclasses = WDClass.from_file()
@@ -165,22 +183,24 @@ def index_ontology(eshost: str):
             "label": str(wdclass.label),
             "readable_label": f"{wdclass.label} ({wdclass.id})",
             "aliases": [str(x) for x in wdclass.aliases],
-            "description": str(wdclass.description)
+            "description": str(wdclass.description),
         }
         for wdclass in wdclasses.values()
     ]
-    docs.append({
-        "id": "http://wikiba.se/ontology#Statement",
-        "uri": "http://wikiba.se/ontology#Statement",
-        "label": "wikibase:Statement",
-        "readable_label": "wikibase:Statement",
-        "aliases": ["statement"],
-        "description": "Wikidata Statement"
-    })
+    docs.append(
+        {
+            "id": "http://wikiba.se/ontology#Statement",
+            "uri": "http://wikiba.se/ontology#Statement",
+            "label": "wikibase:Statement",
+            "readable_label": "wikibase:Statement",
+            "aliases": ["statement"],
+            "description": "Wikidata Statement",
+        }
+    )
     OntologyClassSearch(eshost).load2index(docs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # db = get_qnode_db("/workspace/sm-dev/grams/data/qnodes.db", proxy=True)
     # url = 'http://www.w3.org/2000/01/rdf-schema#label'
     # print(url in db)
